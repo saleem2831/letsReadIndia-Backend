4cfa3dea499d6fc6a1e384a6208359ba33397e34
Shaik Saleem <shaiksaleem2831@gmail.com>
2026-04-19 01:24:59 +0530

    cors added

--- server.js
+++ server.js
@@ -20,6 +20,31 @@ dotenv.config();
 
 const app = express();
 
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://letsreadindia.in',
+  'https://letsreadindia.in'
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, curl, postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // if you're using cookies/auth headers
+  })
+);
+
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
